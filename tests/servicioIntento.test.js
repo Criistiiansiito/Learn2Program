daea9b9128a078b2b306ceff28a8a341bd60f7fb
Sergio <jejozurron@gmail.com>
2025-03-31 01:00:35 +0200

    Anyade test de unidad terminarIntento

--- tests/servicioIntento.test.js
+++ tests/servicioIntento.test.js
@@ -3,14 +3,15 @@ const Test = require('../modelos/Test');
 const Pregunta = require('../modelos/Pregunta');
 const IntentoTest = require('../modelos/IntentoTest');
 const IntentoPregunta = require('../modelos/IntentoPregunta');
-const { TestNoEncontradoError } = require('../utils/errores');
+const { TestNoEncontradoError, IntentoTestNoEncontradoError } = require('../utils/errores');
 
 jest.mock("../modelos/Test", () => ({
     findByPk: jest.fn() // Indicamos que la llamada a findByPk debe ser similada
 }));
 
 jest.mock("../modelos/IntentoTest", () => ({
-    create: jest.fn() // La llamada a create será simulada
+    create: jest.fn(), // La llamada a create será simulada
+    findByPk: jest.fn(),
 }));
 
 describe("intentarTest", () => {
@@ -22,7 +23,7 @@ describe("intentarTest", () => {
         // ## Given ##
         const idTest = 1;
         const mockTest = { preguntas: [] };
-        const mockIntentoTest = {};
+        const mockIntentoTest = { id: 1 };
         Test.findByPk.mockResolvedValue(mockTest);
         IntentoTest.create.mockResolvedValue(mockIntentoTest);
 
@@ -30,14 +31,12 @@ describe("intentarTest", () => {
         const intentoTestId = await servicioIntento.intentarTest(idTest);
 
         // ## Then ##
-        expect(intentoTestId).toBe(mockIntentoTest.id); 
+        expect(intentoTestId).toBe(mockIntentoTest.id);
 
-        expect(Test.findByPk).toHaveBeenCalledWith(idTest, {
-            include: [{ model: Pregunta, as: "preguntas" }]
-        });
+        expect(Test.findByPk).toHaveBeenCalledWith(idTest, expect.any(Object));
         expect(IntentoTest.create).toHaveBeenCalledWith(
             {
-                idTest: idTest,
+                idTest: 1,
                 intentos_pregunta: []
             },
             {
@@ -56,11 +55,53 @@ describe("intentarTest", () => {
         const idTest = 99;
         Test.findByPk.mockResolvedValue(null);
 
-        await expect(servicioIntento.intentarTest(idTest)).rejects.toThrow(TestNoEncontradoError);
+        await expect(servicioIntento.intentarTest(idTest)).rejects.toThrow(new TestNoEncontradoError(idTest));
         expect(Test.findByPk).toHaveBeenCalledWith(idTest, {
-            include: [{ model: Pregunta, as: "preguntas"}]
+            include: [{ model: Pregunta, as: "preguntas" }]
         });
         expect(IntentoTest.create).not.toHaveBeenCalled();
     });
 
+})
+
+describe("terminarIntento", () => {
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    test("deberia actualizar el intento de test y devolver el id del curso", async () => {
+        // ## Given ##
+        const idIntentoTest = 1;
+        const mockIntentoTest = {
+            test: { idCurso: 5 },
+            intentos_pregunta: [
+                { respuesta: { esCorrecta: false } },
+                { respuesta: { esCorrecta: true } },
+                { respuesta: { esCorrecta: true } }
+            ],
+            save: jest.fn().mockResolvedValue(true)
+        };
+        IntentoTest.findByPk.mockResolvedValue(mockIntentoTest);
+        // ## When ##
+        const idCurso = await servicioIntento.terminarIntento(idIntentoTest);
+        // ## Then ##
+        expect(IntentoTest.findByPk).toHaveBeenCalledWith(idIntentoTest, expect.any(Object));
+        expect(mockIntentoTest.preguntasAcertadas).toBe(2);
+        expect(mockIntentoTest.nota).toBe('6.67');
+        expect(mockIntentoTest.terminado).toBe(true);
+        expect(mockIntentoTest.fechaFin).toBeInstanceOf(Date);
+        expect(mockIntentoTest.save).toHaveBeenCalled();
+        expect(idCurso).toBe(mockIntentoTest.test.idCurso);
+    })
+
+    test("deberia lanzar una excepcion cuando no se encuentre el test", async () => {
+        const idIntentoTest = 99;
+        IntentoTest.findByPk.mockResolvedValue(null);
+
+        await expect(servicioIntento.terminarIntento(idIntentoTest))
+            .rejects
+            .toThrow(new IntentoTestNoEncontradoError(idIntentoTest));
+
+        expect(IntentoTest.findByPk).toHaveBeenCalledWith(idIntentoTest, expect.any(Object));
+    })
 })
